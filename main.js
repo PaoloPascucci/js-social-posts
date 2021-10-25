eb2a9caf16af088724533a76f7abf905c059f85d
PaoloPascucci <pascuccip9@gmail.com>
2021-10-25 16:38:05 +0200

    Add Esercizio Finito (no bonus)

--- main.js
+++ main.js
@@ -17,13 +17,39 @@ const Object = [
     {
         id: '2',
         content: 'Placeat libero ipsa quibusdam quas harum ut. Distictio minima iusto. Ad ad maiores et sint voluptate reusandae architrcto. Et nihil ullam aut alias.',
-        image: 'https://picsum.photos/600/350',
+        image: 'https://picsum.photos/601/350',
         date: '2 mesi fa',
         Nlikes: 100,
         name: 'Sofia Perlari',
-        avatar: 'https://picsum.photos/50'
+        avatar: 'https://picsum.photos/51'
     }
 ];
 
 const PostLiked = [];
+
+for (let i = 0; i < Object.length; i++) {
+    const Post = Object[i];
+
+    document.querySelector('.container').innerHTML += `
+    <div class="card">
+    <div class="container">
+    <div class="row">
+        <div class="col-1 py-3"><img class="rounded-circle " src="${Post.avatar}" alt=""></div>
+        <div class="col-11">
+            <h1 class="fs-3 px-1">${Post.name}</h1>
+            <span class="px-1">${Post.date}</span>
+        </div>
+        <div class="col-12">${Post.content}</div>
+                        </div>
+    <div class="card-img"><img src="${Post.image}" alt=""></div>
+    <div class="row card_text gx-0">
+        <div class="col fw-bold"><i class="fas fa-thumbs-up"></i>Mi Piace</div>
+        <div class="col fw-bold">Piace a ${Post.Nlikes} persone</div>
+    </div>
+    </div>
+    </div>
+`
+}
+
+
 
